9c3659342875a80a2943bed88e590cdbb8f51186
brfrec <charloutre34@gmail.com>
2025-05-19 21:26:20 +0200

    Tout remplacer et recommencer pour apprendre

--- classe.py
+++ classe.py
@@ -1,47 +1,31 @@
 # -*- coding: utf-8 -*-
-# Création de la classe 'gateau'
-class gateau:
-    def __init__(self, parfum, taille):# self + recette de la classe 'gateau
-        self.parfum = parfum
-        self.taille = taille
 
-    def CouperEnPpart(self, part = 4):# Que fait on avec ce 'gateau'
-        self.part = part
-        print("Le gateau est coupé en ", self.part, "parts")
+class Personnage:
+    def __init__ (self, nom, genre, pdv):
+        self.nom = nom
+        self.genre = genre
+        self.pdv = pdv
+        
+    def Attaque(self, degats ):
+        self.degats = degats
 
-# Création d'un objet 'gateau' au chocolat de grande taille 
+    def Defence(self, soin):
+        self.soin = soin
 
-gateauChoc = gateau("chocolat", "grand")
-print ("Le ", (gateauChoc.taille), "gateau est au ", (gateauChoc.parfum))
-      
-gateauChoc.CouperEnPpart(6)# Le grand 'gateau' sera coupé en 6 parts
-
-gateauBanane = gateau("banane", "minuscule")#Création d'un objet minuscule 'gateau' a la banane
-print ("Le ", (gateauBanane.taille), "gateau est a la ", (gateauBanane.parfum))
-gateauBanane.CouperEnPpart(2)# Coupé en deux parts
-
-class Voiture:
-    def __init__(self, marque, couleur, puissance, proprietaire ):
-        self.marque = marque
-        self.couleur = couleur
-        self.puissance = puissance
-        self.proprietaire = proprietaire
-
-    def Accelerer(self, vitesse):
-        self.vitesse = vitesse
-        print("La voiture ", (self.marque), "accélère à", (self.vitesse), "km/h") 
-    
-    def Freiner(self, vitesse):
-        self.vitesse = vitesse
-        print("La voiture ", (self.marque), "freine à", (self.vitesse), "km/h")
-
-QL = Voiture("Renault 4L", "orange", "180 ch", "appartient a charlou")
-print("La voiture est une ", (QL.marque), "de couleur ", (QL.couleur), "et de puissance ",
-       (QL.puissance), "et elle appartient à ", (QL.proprietaire))
-QL.Accelerer(280)
-QL.Freiner(0)
-rseize = Voiture("Renault 16", "bleue", "25 ch", "appartient a boti")
-print("La voiture est une ", (rseize.marque), "de couleur ", (rseize.couleur), 
-      "et de puissance ", (rseize.puissance), "et elle ", (rseize.proprietaire))   
-rseize.Accelerer(70)
-rseize.Freiner(10)
+Charloutre = Personnage("Charloutre", "Loutre divine", 250)
+Charloutre.Attaque(20)
+Charloutre.Defence(5)
+print("Nom:",Charloutre.nom)
+print("Héros:",Charloutre.genre)
+print(Charloutre.pdv, "Points de vie") 
+Charloutre.Attaque(20)
+print("Attaque:",Charloutre.degats, "Dégâts")
+Sandyne = Personnage("Sandyne", "Fleur saucisse", 200)
+print("Nom:",Sandyne.nom)
+print("Héros:",Sandyne.genre)
+print(Sandyne.pdv, "Points de vie")
+Charloutre.Attaque(20)
+Sandyne.Defence(4)
+print("Soin:",Sandyne.soin, "Points de vie")
+Sandyne.pdv =Sandyne.pdv - (Charloutre.degats -Sandyne.soin)
+print("Il reste",Sandyne.pdv, "Points de vie")  
