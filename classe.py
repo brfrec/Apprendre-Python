ac7f7a576a983e3cbc64d09920670355367e168a
brfrec <charloutre34@gmail.com>
2025-05-20 11:45:27 +0200

    Plus qu'a améliorer le programme. Fonctionne parfaitement

--- classe.py
+++ classe.py
@@ -13,20 +13,21 @@ class Personnage:
     def Defence(self, soin, echec = 0):
         self.soin = soin
         self.echec = echec
-
+# Personnage 1
 print("HEROS 1 :")
 Charloutre = Personnage("Charloutre", "Loutre divine", 250)
 print("Nom:",Charloutre.nom)
 print("Héros:",Charloutre.genre)
 print(Charloutre.pdv, "Points de vie") 
+# Personnage 2
 print("HEROS 2 :")
 Sandyne = Personnage("Sandyne", "Fleur saucisse", 200)
 print("Nom:",Sandyne.nom)
 print("Héros:",Sandyne.genre)
 print(Sandyne.pdv, "Points de vie")
-Charloutre.Attaque("morsure vivace", (20))
 
- # Attaque de Charloutre
+# Attaque de Charloutre
+Charloutre.Attaque("morsure vivace", (20))
 print (Charloutre.genre, "attaque", Sandyne.genre, "avec", Charloutre.nom, 
        "et cause",Charloutre.degats, "points de dégâts")
 # Défense de Sandyne
@@ -39,3 +40,24 @@ Sandyne.pdv = Sandyne.pdv - Charloutre.degats + Sandyne.echec
 Sandyne.echec
 print(Sandyne.genre, "a", Sandyne.pdv, "points de vie")
 print(Charloutre.genre, "a", Charloutre.pdv, "points de vie")
+
+# Attaque de Sandyne
+Sandyne.Attaque("pollen boudinné", (15))
+print (Sandyne.genre, "attaque", Charloutre.genre, "avec", Sandyne.nom,
+       "et cause",Sandyne.degats, "points de dégâts")
+# Défense de Charloutre
+Charloutre.Defence(5, -1)  
+print (Charloutre.genre, "utilise défence, et se soigne de", Charloutre.soin,
+        "points de vie")   
+print("Défense de loutre divine echoue et cause empoisonnement", Charloutre.genre, "perd 1 points de vie")
+# Mise a jour des points de vie 
+Charloutre.pdv = Charloutre.pdv - Sandyne.degats + Charloutre.echec
+Charloutre.echec
+print(Sandyne.genre, "a", Sandyne.pdv, "points de vie") 
+print(Charloutre.genre, "a", Charloutre.pdv, "points de vie")
+if Sandyne.pdv < Charloutre.pdv:
+    print(Sandyne.genre, "a perdu le combat")
+elif Sandyne.pdv > Charloutre.pdv:
+    print(Charloutre.genre, "a perdu le combat")
+# Fin du combat
+print("Fin du combat")    
